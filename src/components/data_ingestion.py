1964b5d074264c325199cb7d5f84f0c7bcc2bbe4
GyanDev <ramved.2018@gamil.com>
2023-10-10 22:26:07 +0530

    logging and exception

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,3 @@
+# Data Ingestion -----> refers to reading data from a source which can be anything.
+# All the code for diving data into train and test dataset for training ML model purpose.
+# All the code for that we will be writing over here in this file.
---
+++ src/components/model_trainer.py
@@ -0,0 +1,3 @@
+# All the training of ML model and relating task code is done over here in this file.
+# We can create a "model_pusher" folder to push the ML model to cloud.But we can just that in this
+# folder too.
